76f7c14778e1b51718b51c4f8cdb0cb88b7285fe
Grace Crandall <graceac9@uw.edu>
2019-04-02 09:48:44 -0700

    attempt to annotate

--- scripts/skyout-annot-goslim.R
+++ scripts/skyout-annot-goslim.R
@@ -1,6 +1,7 @@
 #=====================================================================================#
 #Script for annotating the Skyline 2015 C. gigas DIA output with GOslim
-
+library(tidyr)
+library(dplyr)
 
 #read in skyline output
 skyout <- read.csv("https://raw.githubusercontent.com/grace-ac/paper-pacific.oyster-larvae/master/data/20181218-SkytoMSstats-report.csv")
@@ -11,7 +12,8 @@ head(skyout_sm)
 
 #read in Blastquery-GOslim.tab
 GO <- read.table("analyses/0401-blastp/Blastquery-GOslim.tab", sep = '\t')
-
+names(GO) <- c("Protein_Name", "GOslim1", "GOslim2", "GOslim3")
+head(GO)
 
 #join files based on "Protein_Name" column in skyout_sm and column 1 in GO
-
+annot <- left_join(skyout_sm, GO, by = "Protein_Name")

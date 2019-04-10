6402fce5e9676acaeab00f73bc75ce841d52e12d
Grace Crandall <graceac9@uw.edu>
2019-04-09 18:28:56 -0700

    make script, join to annotate, write.tab

---
+++ scripts/04-join-annotate-diffexp-prot.R
@@ -0,0 +1,10 @@
+library(dplyr)
+
+prot <- read.table("analyses/0409/_prot-comp-final.tab")
+
+go <- read.table("analyses/0409/Blastquery-GOslim.tab", sep = '\t')
+
+#join two files based on first column
+annot <- left_join(prot, go, "V1")
+
+write.table(annot, "analyses/diffexp-prot-annotated.tab")

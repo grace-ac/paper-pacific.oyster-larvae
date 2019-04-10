84fca5293e53decb1e6be79913ee974f93fc8fc8
Grace Crandall <graceac9@uw.edu>
2019-04-10 10:42:34 -0700

    change to csv

--- scripts/04-join-annotate-diffexp-prot.R
+++ scripts/04-join-annotate-diffexp-prot.R
@@ -7,4 +7,4 @@ go <- read.table("analyses/0409/Blastquery-GOslim.tab", sep = '\t')
 #join two files based on first column
 annot <- left_join(prot, go, "V1")
 
-write.table(annot, "analyses/diffexp-prot-annotated.tab")
+write.csv(annot, "analyses/diffexp-prot-annotated.csv")

995ee146f5953ec7101f48753ba87fb18a3db945
Grace Crandall <graceac9@uw.edu>
2019-09-27 13:31:13 -0700

    edit

--- scripts/04-23and29-GO-enrich-for-Revigo.R
+++ scripts/04-23and29-GO-enrich-for-Revigo.R
@@ -12,8 +12,8 @@ highCGO <- read.table("https://raw.githubusercontent.com/grace-ac/paper-pacific.
 
 #select Go terms and fold enrichment columns for 23C and write out to anaylses
 lowforRev <- select(lowCGO, "Term", "Fold.Enrichment")
-write.csv(lowforRev, "analyses/23C-GOterm-Foldenrich-for-Revigo.csv")
+write.csv(lowforRev, "analyses/23C-GOterm-Foldenrich-for-Revigo.csv", quote = FALSE, row.names = FALSE)
 
 #select Go terms and fold enrichment columns for 29C and write out to anaylses
 highforRev <- select(highCGO, "Term", "Fold.Enrichment")
-write.csv(highforRev, "analyses/29C-GOterm-Foldenrich-for-Revigo.csv")
+write.csv(highforRev, "analyses/29C-GOterm-Foldenrich-for-Revigo.csv", quote = FALSE, row.names = FALSE)

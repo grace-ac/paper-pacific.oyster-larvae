16ca00ebe8c3b5ac20397ebff7719f58e51899af
Grace Crandall <graceac9@uw.edu>
2021-06-14 11:32:50 -0700

    re-do with 2808 detected proteins list instead of the 69 diff. abundant list

--- scripts/03-join-annotate-diffexp-prot.R
+++ scripts/03-join-annotate-diffexp-prot.R
@@ -1,7 +1,7 @@
 library(dplyr)
 
 #Read in differentially abundant proteins --> 69 proteins; threshold (from jupyter noteboook: [02-detected-prots-annotate-GOslim.ipynb](https://github.com/grace-ac/paper-pacific.oyster-larvae/blob/master/notebooks/02-detected-prots-annotate-GOslim.ipynbhttps://github.com/grace-ac/paper-pacific.oyster-larvae/blob/master/notebooks/02-detected-prots-annotate-GOslim.ipynb))    
-prot <- read.table("analyses/BLAST-to-GOslim/_prot-comp-final.tab")
+prot <- read.table("analyses/BLAST-to-GOslim/_prot-final.tab")
 #rename column "V4" "uniprot_acc", and "V1" "protein"
 colnames(prot) <- c("protein", "V2", "V3", "uniprot_acc", "V5", "V6", "V7")
 
@@ -14,7 +14,8 @@ colnames(blastGO) <- c("protein", "GO_ID", "GOslim", "V4")
 #`join` prot and blastGO by "protein":
 protGOslim <- left_join(prot, blastGO, by = "protein")
 
-#575 rows instead of 69. There are multiple GOslim IDs. Just save the biogical process ones ("P" in column "V4")
+#49983 rows instead of 2808. There are multiple GOslim IDs. Just save the biogical process ones ("P" in column "V4")
 
 write.table(protGOslim, "analyses/BLAST-to-GOslim/protGOslim.tab", sep = '\t', row.names = FALSE, quote = FALSE)
 
+#Go back to [02-detected-prots-annotate-GOslim.ipynb](https://github.com/grace-ac/paper-pacific.oyster-larvae/blob/master/notebooks/02-detected-prots-annotate-GOslim.ipynbhttps://github.com/grace-ac/paper-pacific.oyster-larvae/blob/master/notebooks/02-detected-prots-annotate-GOslim.ipynb) to reduce rows and get a table for a pie chart (**Fig. 2** in paper)

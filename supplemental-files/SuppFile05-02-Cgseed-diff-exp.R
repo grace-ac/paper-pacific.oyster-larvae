dcf9a83c3fa05d9be4ff2711df16cb4b12c56dd9
Grace Crandall <graceac9@uw.edu>
2022-03-08 09:29:56 -0800

    replace with newer version

--- supplemental-files/SuppFile05-02-Cgseed-diff-exp.R
+++ supplemental-files/SuppFile05-02-Cgseed-diff-exp.R
@@ -1,10 +1,11 @@
 #Script to subset the proteins that were detected with a log2FC greater than 2.00 and less than -2.00. These are differentially expressed, but not significantly.
 
 #read in annotated protein list that were detected in MSstats
-data <- read.csv("analyses/Cgseed-protcomp-annotation-name.sorted", sep = '\t', header = F)
+data <- read.csv("analyses/annotated-prot-lists/Cgseed-protcomp-annotation-name.sorted", sep = '\t', header = F)
 
 library(dplyr)
 library(tidyr)
+library(stringr)
 
 #subset the proteins that were detected with log2FC greater than 3.00
 diffexup <- subset.data.frame(data, V3 > 2.00)
@@ -15,6 +16,6 @@ diffexlow <- subset.data.frame(data, V3 < -2.00)
 diffex <- rbind(diffexlow, diffexup)
 
 #add column names
-#colnames(diffex) <- c("Protein", "Label", "log2FC", )
+colnames(diffex) <- c("Protein", "Label", "log2FC", "uniprot_accession_ID", "gene_ID", "sprot_uniprotaccession_geneID", "e-value")
   
-write.csv(diffex, "analyses/proteins_comp_annot_threshold.csv")
+write.csv(diffex, "analyses/annotated-prot-lists/proteins_comp_annot_threshold.csv", row.names = FALSE)

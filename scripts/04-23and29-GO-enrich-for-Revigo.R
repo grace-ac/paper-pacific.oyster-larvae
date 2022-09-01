e8d6ac7c2b124db2ae2fb22685e3b5ef17d02103
Grace Crandall <graceac9@uw.edu>
2022-09-01 11:57:44 -0700

    fix paths to files and output

--- scripts/04-23and29-GO-enrich-for-Revigo.R
+++ scripts/04-23and29-GO-enrich-for-Revigo.R
@@ -12,17 +12,17 @@ highCGO <- read.table("https://raw.githubusercontent.com/grace-ac/paper-pacific.
 
 #select Go terms and fold enrichment columns for 23C and write out to anaylses
 lowforRev <- select(lowCGO, "Term", "Fold.Enrichment")
-write.csv(lowforRev, "analyses/23C-GOterm-Foldenrich-for-Revigo.csv", quote = FALSE, row.names = FALSE)
+write.csv(lowforRev, "analyses/revigo/23C-GOterm-Foldenrich-for-Revigo.csv", quote = FALSE, row.names = FALSE)
 
 #select Go terms and fold enrichment columns for 29C and write out to anaylses
 highforRev <- select(highCGO, "Term", "Fold.Enrichment")
-write.csv(highforRev, "analyses/29C-GOterm-Foldenrich-for-Revigo.csv", quote = FALSE, row.names = FALSE)
+write.csv(highforRev, "analyses/revigo/29C-GOterm-Foldenrich-for-Revigo.csv", quote = FALSE, row.names = FALSE)
 
 # create one for both temperatures combined:
 # enriched GO terms from DAVID of the 69 differentially abundant proteins and associated fold enrichment:
 # read in david output for enrichment of 69 differentially abundant proteins:
-dap <- read.delim("analyses/david-comp-mstat.txt", sep = '\t')
+dap <- read.delim("analyses/DAVID-outputs/david-comp-mstat.txt", sep = '\t')
 
 #select Term and Fold enrichment and write out to analsyes:
 dap_for_rev <- select(dap, "Term", "Fold.Enrichment")
-write.csv(dap_for_rev, "analyses/enriched_diff.ab.proteins-for-Revigo.csv", quote = FALSE, row.names = FALSE)
+write.csv(dap_for_rev, "analyses/revigo/enriched_diff.ab.proteins-for-Revigo.csv", quote = FALSE, row.names = FALSE)
